9b3b5f85165bdda8710eaa0846f14a6b19b15224
wangww <372266373@qq.com>
2020-08-04 16:42:54 +0800

    半屏扫码

--- app/src/main/java/com/kaidongyuan/app/kdydriver/ui/activity/ScanActivity.java
+++ app/src/main/java/com/kaidongyuan/app/kdydriver/ui/activity/ScanActivity.java
@@ -1,14 +1,65 @@
 package com.kaidongyuan.app.kdydriver.ui.activity;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.KeyEvent;
 
 import com.journeyapps.barcodescanner.CaptureActivity;
+import com.journeyapps.barcodescanner.CaptureManager;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+import com.kaidongyuan.app.kdydriver.R;
 
 public class ScanActivity extends CaptureActivity {
+
+    private CaptureManager capture;
+    private DecoratedBarcodeView barcodeScannerView;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.scan);
+
+        barcodeScannerView = (DecoratedBarcodeView) findViewById(R.id.dbv_custom);
+
+        capture = new CaptureManager(this, barcodeScannerView);
+        capture.initializeFromIntent(getIntent(), savedInstanceState);
+        capture.decode();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        capture.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        capture.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        capture.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        capture.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        capture.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return barcodeScannerView.onKeyDown(keyCode, event) || super.onKeyDown(keyCode, event);
     }
 }
