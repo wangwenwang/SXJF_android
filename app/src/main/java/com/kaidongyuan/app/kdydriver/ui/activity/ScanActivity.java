13242b34b22ada3a75cc2503be8bcb6f70ff8f38
wangww <372266373@qq.com>
2020-08-08 19:02:24 +0800

    拍照解决了一半，版本更新解决了一半

--- app/src/main/java/com/kaidongyuan/app/kdydriver/ui/activity/ScanActivity.java
+++ app/src/main/java/com/kaidongyuan/app/kdydriver/ui/activity/ScanActivity.java
@@ -1,17 +1,24 @@
 package com.kaidongyuan.app.kdydriver.ui.activity;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Looper;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.FileProvider;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
@@ -27,13 +34,30 @@ import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.alibaba.fastjson.JSONObject;
+import com.allenliu.versionchecklib.v2.AllenVersionChecker;
+import com.allenliu.versionchecklib.v2.builder.UIData;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 import com.journeyapps.barcodescanner.CaptureActivity;
 import com.journeyapps.barcodescanner.CaptureManager;
 import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+import com.kaidongyuan.app.basemodule.utils.nomalutils.MPermissionsUtil;
 import com.kaidongyuan.app.kdydriver.R;
 import com.kaidongyuan.app.kdydriver.bean.Tools;
+import com.kaidongyuan.app.kdydriver.constants.Constants;
+
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static android.widget.Toast.LENGTH_LONG;
 
@@ -55,7 +79,16 @@ public class ScanActivity extends CaptureActivity {
     private ValueCallback<Uri> uploadMessage;
     // 5.0及以上使用
     private ValueCallback<Uri[]> uploadMessageAboveL;
-
+    //图片
+    private final static int FILE_CHOOSER_RESULT_CODE = 128;
+    //拍照
+    private final static int FILE_CAMERA_RESULT_CODE = 129;
+    //拍照图片路径
+    private String cameraFielPath;
+    private final int RequestPermission_STATUS_CODE0=8800;
+    private static final String FILE_PROVIDER_AUTHORITY = "com.kaidongyuan.qh_orders_android.fileprovider";
+    private Uri mImageUri;
+    public static final int REQUEST_READ_PHONE_STATE = 8;
 
     @SuppressLint("JavascriptInterface")
     @Override
@@ -114,26 +147,26 @@ public class ScanActivity extends CaptureActivity {
             // For Android < 3.0
             public void openFileChooser(ValueCallback<Uri> valueCallback) {
                 uploadMessage = valueCallback;
-//                openImageChooserActivity();
+                openImageChooserActivity();
             }
 
             // For Android  >= 3.0
             public void openFileChooser(ValueCallback valueCallback, String acceptType) {
                 uploadMessage = valueCallback;
-//                openImageChooserActivity();
+                openImageChooserActivity();
             }
 
             //For Android  >= 4.1
             public void openFileChooser(ValueCallback<Uri> valueCallback, String acceptType, String capture) {
                 uploadMessage = valueCallback;
-//                openImageChooserActivity();
+                openImageChooserActivity();
             }
 
             // For Android >= 5.0
             @Override
             public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, WebChromeClient.FileChooserParams fileChooserParams) {
                 uploadMessageAboveL = filePathCallback;
-//                openImageChooserActivity();
+                openImageChooserActivity();
                 return true;
             }
 
@@ -199,6 +232,254 @@ public class ScanActivity extends CaptureActivity {
             }
         });
         capture.decode();
+//        initPermission();
+    }
+
+    private void openImageChooserActivity() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(ScanActivity.this);
+        builder.setTitle("拍照/相册");
+        builder.setPositiveButton("相册", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                takePhoto();
+            }
+        });
+        builder.setNegativeButton("拍照", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                takeCamera();
+            }
+        });
+        builder.setCancelable(false);
+        builder.show();
+    }
+
+    //选择图片
+    private void takePhoto() {
+        Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+        i.addCategory(Intent.CATEGORY_OPENABLE);
+        i.setType("image/*");
+        startActivityForResult(Intent.createChooser(i, "Image Chooser"), FILE_CHOOSER_RESULT_CODE);
+    }
+
+    //拍照
+    private void takeCamera() {
+       /* String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_"+timeStamp+"_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File imageFile;
+        try {
+
+            imageFile = File.createTempFile(imageFileName,".jpg",storageDir);
+            cameraFielPath = imageFile.getPath();
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+        File cameraPhoto = new File(cameraFielPath);
+        Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        Uri photoUri = FileProvider.getUriForFile(
+                this,
+                "com.cy_scm.tms_android.fileprovider",
+                cameraPhoto);
+        takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+        startActivityForResult(takePhotoIntent, FILE_CAMERA_RESULT_CODE);
+
+
+*/
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (hasSDCard()) {
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            String imageFileName = "JPEG_"+timeStamp+"_";
+            File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+            File imageFile;
+            try {
+
+                imageFile = File.createTempFile(imageFileName,".jpg",storageDir);
+                cameraFielPath = imageFile.getPath();
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            }
+            File outputImage = new File(cameraFielPath);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(outputImage));
+            startActivityForResult(intent, FILE_CAMERA_RESULT_CODE);
+        }
+    }
+
+    /**
+     * 判断手机是否有SD卡。
+     *
+     * @return 有SD卡返回true，没有返回false。
+     */
+    public boolean hasSDCard() {
+
+        return Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState());
+    }
+
+    private void takeCameraM() {
+        Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);//打开相机的Intent
+        if(takePhotoIntent.resolveActivity(getPackageManager())!=null){//这句作用是如果没有相机则该应用不会闪退，要是不加这句则当系统没有相机应用的时候该应用会闪退
+            File imageFile = createImageFile();//创建用来保存照片的文件
+            if(imageFile!=null){
+                if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.N){
+//                    7.0以上要通过FileProvider将File转化为Uri
+                    mImageUri = FileProvider.getUriForFile(this,FILE_PROVIDER_AUTHORITY,imageFile);
+                }else {
+//                    7.0以下则直接使用Uri的fromFile方法将File转化为Uri
+                    mImageUri = Uri.fromFile(imageFile);
+                }
+                takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT,mImageUri);//将用于输出的文件Uri传递给相机
+                startActivityForResult(takePhotoIntent, FILE_CAMERA_RESULT_CODE);//打开相机
+            }
+        }else {
+        }
+    }
+
+    /**
+     * 创建用来存储图片的文件，以时间来命名就不会产生命名冲突
+     * @return 创建的图片文件
+     */
+    private File createImageFile() {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_"+timeStamp+"_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File imageFile = null;
+        try {
+            imageFile = File.createTempFile(imageFileName,".jpg",storageDir);
+            cameraFielPath = imageFile.getPath();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return imageFile;
+    }
+
+    private void initPermission() {
+
+        Log.d("LM", "申请存储权限");
+
+        try {
+
+            if (Build.VERSION.SDK_INT>=23){
+                if (MPermissionsUtil.checkAndRequestPermissions(ScanActivity.this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,
+                                Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE}
+                        ,RequestPermission_STATUS_CODE0)){
+
+
+                    new Thread() {
+                        public void run() {
+
+                            checkVersion();
+                        }
+                    }.start();
+                }
+            }else {
+
+                checkVersion();
+            }
+        }catch (Exception e) {
+
+            Log.d("LM", "initPermission: " + e.getMessage());
+        }
+    }
+
+    public void checkVersion() {
+
+        Log.d("LM", "检查apk及zip版本");
+
+        String params = "{\"tenantCode\":\"SFKC\"}";
+        String paramsEncoding = URLEncoder.encode(params);
+        String Strurl = Constants.URL.SAAS_API_BASE + "queryAppVersion.do?params=" + paramsEncoding;
+        HttpURLConnection conn = null;
+        try {
+
+            URL url = new URL(Strurl);
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setConnectTimeout(5000);
+            conn.setRequestMethod("GET");
+            if (HttpURLConnection.HTTP_OK == conn.getResponseCode()) {
+
+                InputStream in = conn.getInputStream();
+                String resultStr = Tools.inputStream2String(in);
+                resultStr = URLDecoder.decode(resultStr, "UTF-8");
+
+                try {
+                    org.json.simple.JSONObject jsonObj = (org.json.simple.JSONObject) (new JSONParser().parse(resultStr));
+                    Log.i("LM", jsonObj.toJSONString() + "\n" + jsonObj.getClass());
+                    String status = (String) jsonObj.get("status");
+                    String Msg = (String) jsonObj.get("Msg");
+
+                    String apkDownloadUrl = null;
+                    String server_apkVersion = null;
+                    if (status.equals("1")) {
+
+                        org.json.simple.JSONObject dict = (org.json.simple.JSONObject) jsonObj.get("data");
+                        apkDownloadUrl = (String) dict.get("downloadUrl");
+                        server_apkVersion = (String) dict.get("versionNo");
+                    }
+                    if (server_apkVersion != null && apkDownloadUrl != null) {
+                        try {
+                            String current_apkVersion = local_Version;
+                            Log.d("LM", "server_apkVersion:" + server_apkVersion + "\tcurrent_apkVersion:" + current_apkVersion);
+                            int compareVersion = Tools.compareVersion(server_apkVersion, current_apkVersion);
+                            if (compareVersion == 1) {
+                                createUpdateDialog(current_apkVersion, server_apkVersion, apkDownloadUrl);
+                            }
+                        } catch (Exception e) {
+                            Log.d("LM", "NameNotFoundException" + e.getMessage());
+                            e.printStackTrace();
+                        }
+                    }
+
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                in.close();
+            } else {
+                Log.d("LM", "检查版本接口|queryAppVersion.do|请求失败");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            conn.disconnect();
+        }
+        Log.d("LM", "checkVersion: ");
+    }
+
+
+    /**
+     * 版本更新对话框
+     *
+     * @param currentVersion 当前版本versionName
+     * @param version        最新版本versionName
+     * @param downUrl        最新版本安装包下载url
+     */
+    public void createUpdateDialog(String currentVersion, String version, final String downUrl) {
+
+
+        String fff = "http://120.77.206.44/CYSCMAPP/mhy/mhy.apk";
+        Log.d("LM", "createUpdateDialog: ");
+
+        AllenVersionChecker
+                .getInstance()
+                .downloadOnly(
+                        UIData.create()
+                                .setDownloadUrl(downUrl)
+                                .setTitle("更新版本")
+                                .setContent("当前版本：" + currentVersion + "\n最新版本：" + version)
+                )
+                .executeMission(mContext);
+
+//        DownloadManager manager = DownloadManager.getInstance(this);
+//        manager.setApkName("appupdate.apk")
+//                .setApkUrl(downUrl)
+//                .setSmallIcon(R.mipmap.ic_launcher)
+//                .download();
+
     }
 
     // js调用java
@@ -260,8 +541,8 @@ public class ScanActivity extends CaptureActivity {
                     public void run() {
 
                         JSONObject lngObject = new JSONObject();
-                        lngObject.put("lng", lng);
-                        lngObject.put("lat", lat);
+                        lngObject.put("lng", 114.2);
+                        lngObject.put("lat", 22.44);
 
                         String url = "javascript:SetCurrAddress('" + lngObject + "')";
 
@@ -369,9 +650,14 @@ public class ScanActivity extends CaptureActivity {
         @JavascriptInterface
         public void updateAPP() {
 
-//            initHandler();
-//            checkVersion();
-//            ifManualUpdate = "手动更新";
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+
+                    checkVersion();
+                }
+            });
+
         }
     }
 
@@ -430,7 +716,29 @@ public class ScanActivity extends CaptureActivity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
-        capture.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//        capture.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Log.d("LM", "拍照5.8: ");
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        Log.d("LM", "拍照5.9: ");
+
+        if (requestCode == 101) {
+
+
+            Log.d("LM", "拍照5.9.1: ");
+            takeCameraM();
+        }
+        switch (requestCode) {
+            case REQUEST_READ_PHONE_STATE:
+                if ((grantResults.length > 0) && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    //TODO
+                }
+                break;
+
+            default:
+                break;
+        }
     }
 
     @Override
